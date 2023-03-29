b84ee926687241a37197093354aafd14dd6b33b4
Ezekielsam <connect2gracealone@gmail.com>
2023-03-29 23:13:19 +0100

    a quick fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
-        """Represent a square."""
+    """Represent a square."""
+    def __init__(self, size):
+        """Initialize a new square.
 
-        def __init__(self, size):
-          """Initialize a new square.
 
-
-          Args:
+        Args:
             size (int): The size of the new square.
-          """
-          self.size = size
+        """
+        self.size = size
 
         @property
         def size(self):
@@ -26,14 +26,14 @@ class Square:
                 raise ValueError("size must be >= 0")
             self.__size = value
 
-        def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
+            def area(self):
+                """Return the current area of the square."""
+                return (self.__size * self.__size)
 
-        def my_print(self):
-            """Print the square with the # character."""
-            for i in range(0, self.__size):
-                [print("#", end="") for j in range(self.__size)]
-                print("")
-            if self.__size == 0:
-                print("")
+            def my_print(self):
+                """Print the square with the # character."""
+                for i in range(0, self.__size):
+                    [print("#", end="") for j in range(self.__size)]
+                    print("")
+                    if self.__size == 0:
+                        print("")
