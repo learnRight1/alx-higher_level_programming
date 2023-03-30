48c70170f7da9444cceea8e5fe65da902b4f96f4
Ezekielsam <connect2gracealone@gmail.com>
2023-03-30 01:11:58 +0100

    updated

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,12 +4,10 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size):
-        """Initialize a new square.
-
-
-        Args:
-            size (int): The size of the new square.
+        """Initialize a new square.Args:
+        size (int): The size of the new square.
         """
         self.size = size
 
